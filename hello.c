774275328f7ec372d084ddbfff4421c283593ff4
Emmanuel Druon <emmanuel.druon@yncrea.fr>
2020-08-17 09:58:49 -0700

    Initial version

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i=100;
+
+    printf("Hello Class\n");
+    printf("i=%d\n",i);
+
+    printf("Enter a new value for i: ");
+    scanf("%d",&i);
+    printf("i=%d\n",i);
+
+    printf("That's all folks!\n");
+
+    return 0;
+} 
